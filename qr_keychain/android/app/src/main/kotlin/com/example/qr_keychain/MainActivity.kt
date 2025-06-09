c10887385c2813f53fb5f9b1909080bf04d2f01c
AleCava01 <52034469+AleCava01@users.noreply.github.com>
2025-06-09 11:49:47 +0200

    Deploy Flutter Web to GitHub Pages

--- qr_keychain/android/app/src/main/kotlin/com/example/qr_keychain/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.qr_keychain
-
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-
-class MainActivity: FlutterFragmentActivity()
